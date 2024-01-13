750c77dfbc3609c95e59dbe8e05b2d913cfc156a
Damitha <hellodamitha@gmail.com>
2024-01-13 17:17:45 +0530

    hallAvailability view created

--- sql views.sql
+++ sql views.sql
@@ -3,3 +3,10 @@ AS
 SELECT name, age FROM Student WHERE (age < 23)
 
 SELECT * FROM View_studentAge;
+
+CREATE VIEW View_hallAvailability
+AS
+SELECT examDate, hall FROM exam 
+WHERE (hall = 'Hall A')
+
+SELECT * FROM View_hallAvailability;
